82e715bb20bccec9330b7547f6e6d67c85988086
Leao <lucasleaolima@gmail.com>
2025-12-05 17:48:46 -0300

    config: varibles no more hardcoded

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,13 +1,15 @@
 import { PrismaMariaDb } from '@prisma/adapter-mariadb'
 import { PrismaClient } from '../../prisma/generated/client'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const adapter = new PrismaMariaDb({
     host: 'localhost',
     port: 3306,
-    user: 'user',
-    password: 'user123',
-    database: 'alunos_db',
-    connectionLimit: 5
+    user: process.env.MYSQL_USER || '',
+    password: process.env.MYSQL_PASSWORD || '',
+    database: process.env.MYSQL_DATABASE || '',
+    connectionLimit: 50
 })
 
 const prisma = new PrismaClient({adapter, transactionOptions: {
